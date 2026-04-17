c070f850a40590dfce33fa1f4e90b68602daf956
Uriel Rojas Quintero <uriel.rojas.q@gmail.com>
2026-04-17 15:35:35 -0500

    idk

---
+++ database/migrations/2026_04_17_175012_create_unidades_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre', 255);
+            //id tipo unidad
+            //$table->unsignedBigInteger('tipo_unidad_id')->nullable();
+            //$table->foreign('tipo_unidad_id')->references('id')->on('tipos_unidades');
+            $table->integer('numero');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unidades');
+    }
+};
---
+++ database/migrations/2026_04_17_175036_create_trabajadores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trabajadores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre', 255);
+            //id tipo trabajador
+            //$table->unsignedBigInteger('tipo_trabajador_id')->nullable();
+            //$table->foreign('tipo_trabajador_id')->references('id')->on('tipos_trabajadores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trabajadores');
+    }
+};
