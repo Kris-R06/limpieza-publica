4bf0a45bb8be0ded67afc8d0e83988569150af9a
Christopher Ortiz Ramos <l22260148@matamoros.tecnm.mx>
2026-04-20 03:30:25 -0500

    Modifiacion en el logo

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>{{ $pageTitle ?? 'sudo-Trash' }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="icon" type="image/png" href="{{ asset('basura.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
 
   {{-- Fuentes --}}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $pageTitle ?? 'sudo-Trash' }}</title>
-    <link rel="icon" type="image/png" href="{{ asset('basura.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
 
     {{-- Fuentes: Estandarizamos a Barlow para todo el proyecto --}}
     <link rel="preconnect" href="https://fonts.googleapis.com" />
